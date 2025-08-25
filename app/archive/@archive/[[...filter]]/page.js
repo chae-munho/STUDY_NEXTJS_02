d2c50da880ab6ac28271faed23234b23229def2e
chae-munho <ansgh4300@naver.com>
2025-08-25 17:48:15 +0900

    서버 vs 클라이언트 진행할 차례

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,67 @@
+import NewsList from "@/components/news-list";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+import Link from "next/link";
+//[[...filter]] : archive 이후 모든 경로 세그먼트에 대해 활성화되도록 보장한다. filter은 단일값이 아니다.
+// [[...filter]] 의미:
+// [[...filter]]는 Optional Catch-all Route
+// 매칭 가능한 URL:
+// /archive → params.filter === undefined 또는 []
+// /archive/2024 → params.filter === ['2024']
+// /archive/2024/news → params.filter === ['2024', 'news']
+
+export default function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error('Invalid filter.');  //error.js 파일 출력하기
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}

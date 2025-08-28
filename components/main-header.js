f73e84e36a6170c1467fec25ed1843dcde72da59
chae-munho <ansgh4300@naver.com>
2025-08-28 23:38:32 +0900

    동적 라우트 안에 중첩된 라우트

--- components/main-header.js
+++ components/main-header.js
@@ -1,7 +1,12 @@
 
+// "use client"; // usePathname() 클라이언트 컴포넌트이므로 use client를 추가해야 된다 근데 해당 컴포넌트를 nav-link.js 로 옮겨짐
+//<NavLink> 컴포넌트로 분리했으므로 해당 페이지에는 use client를 사용할 필요 없다 이젠 기본적으로 use server 상태가 되는거임
 import Link from "next/link";
+import NavLink from "./nav-link";
+
 
 export default function MainHeader() {
+    
     return (
         <header id="main-header">
             <div id="logo">
@@ -10,7 +15,11 @@ export default function MainHeader() {
             <nav>
                 <ul>
                     <li>
-                        <Link href="/news">News</Link>
+                        <NavLink href="/news">News</NavLink>
+                    </li>
+                       
+                      <li>
+                         <NavLink href="/archive">Archive</NavLink>
                     </li>
                 </ul>
             </nav>
